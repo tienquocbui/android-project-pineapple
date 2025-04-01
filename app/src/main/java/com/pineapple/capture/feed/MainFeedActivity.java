d810964dcdc985dea57f674421144dedb6622236
Debojyoti Mishra <105543436+DebojyotiMishra@users.noreply.github.com>
2025-04-01 15:15:12 +0200

    feat: add logout and minor style fixes

--- app/src/main/java/com/pineapple/capture/feed/MainFeedActivity.java
+++ app/src/main/java/com/pineapple/capture/feed/MainFeedActivity.java
@@ -1,21 +1,29 @@
 package com.pineapple.capture.feed;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.pineapple.capture.R;
+import com.pineapple.capture.activities.LoginActivity;
 
 public class MainFeedActivity extends AppCompatActivity {
     private MainFeedViewModel viewModel;
     private RecyclerView recyclerView;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_feed);
 
+        mAuth = FirebaseAuth.getInstance();
         viewModel = new ViewModelProvider(this).get(MainFeedViewModel.class);
         
         recyclerView = findViewById(R.id.feed_recycler_view);
@@ -25,5 +33,28 @@ public class MainFeedActivity extends AppCompatActivity {
         viewModel.getFeedItems().observe(this, feedItems -> {
             // Update RecyclerView adapter with new items
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        mAuth.signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 } 
