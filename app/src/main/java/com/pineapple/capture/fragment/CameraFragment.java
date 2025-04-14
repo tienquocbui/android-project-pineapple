0bc6f1b9f3d156fcb20fa22e50c5fc67d2fdf6fb
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2025-04-14 22:24:16 +0200

    feat(camera): update some UI for the camera fragment

--- app/src/main/java/com/pineapple/capture/fragment/CameraFragment.java
+++ app/src/main/java/com/pineapple/capture/fragment/CameraFragment.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
@@ -33,6 +32,8 @@ public class CameraFragment extends Fragment {
     private PreviewView previewView;
     private ImageCapture imageCapture;
     private boolean flashEnabled = false;
+    private ImageButton toggleFlashButton;
+
 
     private boolean isUsingFrontCamera = false;
 
@@ -55,15 +56,13 @@ public class CameraFragment extends Fragment {
             startCamera();
         });
 
-        Button cancelButton = view.findViewById(R.id.cancel_button);
-        cancelButton.setOnClickListener(v -> requireActivity().onBackPressed());
-
         ImageButton captureButton = view.findViewById(R.id.capture_button);
         captureButton.setOnClickListener(v -> captureImage());
 
-        ImageButton toggleFlashButton = view.findViewById(R.id.toggle_flash);
+        toggleFlashButton = view.findViewById(R.id.toggle_flash);
         toggleFlashButton.setOnClickListener(v -> {
             flashEnabled = !flashEnabled;
+            updateFlashIconColor();
             startCamera();
         });
 
@@ -97,7 +96,7 @@ public class CameraFragment extends Fragment {
 
                 cameraProvider.unbindAll();
                 cameraProvider.bindToLifecycle(
-                        this, cameraSelector, preview, imageCapture
+                        getViewLifecycleOwner(), cameraSelector, preview, imageCapture
                 );
 
             } catch (ExecutionException | InterruptedException e) {
@@ -105,6 +104,15 @@ public class CameraFragment extends Fragment {
             }
         }, ContextCompat.getMainExecutor(requireContext()));
     }
+
+    private void updateFlashIconColor() {
+        int color = flashEnabled
+                ? ContextCompat.getColor(requireContext(), android.R.color.white)
+                : ContextCompat.getColor(requireContext(), R.color.primary_blue);
+
+        toggleFlashButton.setColorFilter(color);
+    }
+
 
     private void captureImage() {
         if (imageCapture == null) return;
