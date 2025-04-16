f82764526a824819966e4292178d61ac3b011640
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2025-04-16 03:01:49 +0200

    feat(storage): set up cloudinary for storing images

--- app/src/main/java/com/pineapple/capture/utils/CloudinaryManager.java
+++ app/src/main/java/com/pineapple/capture/utils/CloudinaryManager.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import com.cloudinary.android.MediaManager;
 import com.cloudinary.android.callback.ErrorInfo;
 import com.cloudinary.android.callback.UploadCallback;
+import com.pineapple.capture.BuildConfig;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,11 +17,16 @@ public class CloudinaryManager {
     private static boolean isInitialized = false;
 
     public static void init(Context context) {
+        String cloudName = BuildConfig.CLOUDINARY_CLOUD_NAME;
+        String apiKey = BuildConfig.CLOUDINARY_API_KEY;
+        String apiSecret = BuildConfig.CLOUDINARY_API_SECRET;
+
+
         if (!isInitialized) {
             Map<String, String> config = new HashMap<>();
-            config.put("cloud_name", "den0bpixy");
-            config.put("api_key", "955574316579734");
-            config.put("api_secret", "WfyFf4y1Wsnxzuf6pRC0KKwxNBI");
+            config.put("cloud_name", cloudName);
+            config.put("api_key", apiKey);
+            config.put("api_secret", apiSecret);
             MediaManager.init(context, config);
             isInitialized = true;
         }
